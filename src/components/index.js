96a9882db272da816ec5ef2c86a415594e85e03f
bqin628 <bruce.qin@outlook.com>
2020-05-04 00:08:23 -0700

    search page update 1

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 // takes all the components and exports them from the components folder
 export { default as Header } from './Header/Header';
+export { default as SearchBox} from './SearchBox/SearchBox';
