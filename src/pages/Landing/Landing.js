15930e50753858d4390da3056cf8fc61b930a510
Jeffrey Luo <jl834@duke.edu>
2020-05-03 18:50:30 -0700

    Landing page now has a button that links to the main page

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,10 +1,20 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Landing = () => {
     return (
         <div>
-            <h1>Drink Match</h1>
-            <h3>Your personal online bartender.</h3>
+            <img src="logo192.png" alt="logo"/>
+            <div>
+                <h1>Drink Match</h1>
+                <h3>Your personal online bartender.</h3>
+                <Link to="/main">
+                    <button type="button">
+                        Click Me!
+                    </button>
+                </Link>
+            </div>
         </div>
     )
 }
