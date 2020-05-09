8d24885c55a4522a25d4f672daad3d0ea4c0ad32
Jeffrey Luo <jeffreyluo@Jeffreys-MBP-2.lan>
2020-05-09 16:01:37 -0700

    API fetches drinks that can be made from gin.

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,22 @@
 import axios from 'axios';
 
 const ingredientsUrl = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list';
+const drinkFetchUrl ='https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Gin';
 
 export const fetchIngredients = async () => {
     try {
         const { data: { drinks }} = await axios.get(ingredientsUrl);
 
         return drinks.map((ingredient) => ({ title: ingredient.strIngredient1 }));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const fetchDrinks = async () => {
+    try {
+        const { data : {drinks} } = await axios.get(drinkFetchUrl);
+        return drinks;
     } catch (error) {
         console.log(error);
     }
--- src/pages/Result/Result.js
+++ src/pages/Result/Result.js
@@ -1,11 +1,11 @@
-import React from 'react';
-
+import React, {useState, useEffect} from 'react';
 import { Header, DrinkCardList } from '../../components';
 import { Typography } from '@material-ui/core';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Fab from '@material-ui/core/Fab';
 import LocalBarTwoToneIcon from '@material-ui/icons/LocalBarTwoTone';
 import { Link } from 'react-router-dom';
+import { fetchDrinks } from '../../api';
 
 const useStyles = makeStyles((theme) => ({
     wrapper: {
@@ -28,10 +28,22 @@ const Result = () => {
     const classes = useStyles();
     const theme = useTheme();
 
+    const [drinks, setDrinks] = useState([]);
+
+    useEffect(() => {
+        const fetchAPI = async () => {
+            setDrinks(await fetchDrinks());
+        }
+        fetchAPI();
+    }, []);
+
+    console.log(drinks);
+
     return (
         <div className={classes.wrapper}>
             <Header />
             <Typography variant="h4" className={classes.padding}>Here are the drinks you can make:</Typography>
+            <DrinkCardList drinks={drinks} />
             <div>
                 <Link to="" className={classes.link} >
                     <Fab color="primary" className={classes.fab} aria-label="Home">
