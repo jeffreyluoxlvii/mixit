0c2a7af1234ea6f2db480ff47d1dc53a8338ad8a
Jeffrey Luo <jeffreyluo@Jeffreys-MBP-2.lan>
2020-05-09 01:19:17 -0700

    Created DrinkCardList

---
+++ src/components/DrinkCardList/DrinkCardList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import DrinkCard from '../../components'
+import { Container } from '@material-ui/core'
+
+const DrinkCardList = ({ drinks }) => {
+    const cardArray = drinks.map((drink, i) => {
+        return (
+            <DrinkCard
+                key={i}
+                name={drinks[i].name}
+                image={drinks[i].image}
+            />
+        );
+    })
+
+    return (
+        <Container>
+            {cardArray}
+        </Container>
+    )
+}
+export default CardList;
