01ba6b11943a6d9cb2a7ce2c12237d1af595e0c2
Jeffrey <jeffrey.luo@duke.edu>
2020-06-27 13:40:26 -0700

    Updates

---
+++ precache-manifest.ae1a503892d2536ed35c6d4d84dfb174.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2302e7158246ee949483d82f7d6214f2",
+    "url": "/mixit/index.html"
+  },
+  {
+    "revision": "7a9ff0d6c6113bf687d7",
+    "url": "/mixit/static/css/main.f7512883.chunk.css"
+  },
+  {
+    "revision": "4c4d331d9c82cbc1acfc",
+    "url": "/mixit/static/js/2.d32e2e3a.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/mixit/static/js/2.d32e2e3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a9ff0d6c6113bf687d7",
+    "url": "/mixit/static/js/main.4c1a4d64.chunk.js"
+  },
+  {
+    "revision": "4a064824822b699610c3",
+    "url": "/mixit/static/js/runtime-main.f6a0c624.js"
+  },
+  {
+    "revision": "f510500271d96bb1952fda2c76e41084",
+    "url": "/mixit/static/media/shakeranddrink.f5105002.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.699c526f368e8be8be9cccc08f7c4c97.js"
+  "/mixit/precache-manifest.ae1a503892d2536ed35c6d4d84dfb174.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mixit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
